02b2d305165f4e8b66d7e9267ab6d6bfc7c7e3eb
Rafael Sousa Silva <rsoussil@emeal.nttdata.com>
2024-05-06 15:39:17 -0300

    inclusão de exercício com switchCase e exercício usando modularização, delegação e reaproveitamento de fuñções

---
+++ src/For_Estruturas_Repetitivas_II2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class For_Estruturas_Repetitivas_II2 {
+public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int soma = 0;
+		
+		for (int i=0; i<x; i++) {
+			soma = soma + x;
+			//soma += x;
+			x = sc.nextInt();			
+		}
+				
+		//System.out.printf("O Valor total dos números inseridos é %d", soma);
+		System.out.println(soma);
+		
+		sc.close();
+	}
+}
